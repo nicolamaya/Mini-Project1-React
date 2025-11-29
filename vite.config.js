0f0982175d930a0423437e04441f705962698048
nicolamaya <199396618+nicolamaya@users.noreply.github.com>
2025-11-29 09:46:58 -0500

    feat: Edit card

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mini-Project1-React/', // 👈 importante: debe tener slash inicial y final
+  base: '/', // 👈 importante: debe tener slash inicial y final
 })
